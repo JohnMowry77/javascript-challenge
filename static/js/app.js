a16c4e79e34b2b636579a5690b66b5aa0c9bf873
John Mowry <72773479+JohnMowry77@users.noreply.github.com>
2021-03-24 20:22:16 -0600

    key value pairs

--- static/js/app.js
+++ static/js/app.js
@@ -9,15 +9,29 @@ var tbody = d3.select('tbody');
 //console.log the weather data 
 console.log(tableData)
 
-//Loop through data and console.log each ufo report object
+//Loop through data and console.log each ufoSightings object
 tableData.forEach(function(ufoSightings) {
 	console.log(ufoSightings);
 });
 
-//Use d3 to append one table row 'tr' for each ufo report object
+//Use d3 to append one table row 'tr' for each ufoSightings object
 tableData.forEach(function(ufoSightings) {
 	console.log(ufoSightings);
 	var row= tbody.append("tr");
 });
 
-//Use Object.entries to console.log each ufo report value
+//Use Object.entries to console.log each ufoSightings value
+tableData.forEach(function(ufoSightings) {
+	console.log(ufoSightings);
+	var row =tbody.append("tr");
+	
+//create a dict
+Object.entries(ufoSightings).forEach(function([k,v]) {
+	console.log(k,v);
+	});
+});
+
+
+
+//Use d3 to append 1 cell per ufoSightings value
+
