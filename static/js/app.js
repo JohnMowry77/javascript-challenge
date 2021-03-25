6b249255ad8c0554241eba8d60696dc266928f49
John Mowry <72773479+JohnMowry77@users.noreply.github.com>
2021-03-24 19:49:48 -0600

    folder setup

---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+//Get a reference to the table body
+var tbody = d3.select('tbody');
+
+//console.log the weather data 
+console.log(tableData)
